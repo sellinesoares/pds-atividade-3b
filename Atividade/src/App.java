903390fc821f8b13472affe34cab0045bef9d45e
sellinesoares <144940454+sellinesoares@users.noreply.github.com>
2024-12-09 17:43:32 -0300

    Add files via upload
    Projeto Pronto!

---
+++ Atividade/src/App.java
@@ -0,0 +1,12 @@
+
+public class App {
+		
+	 public static void main(String[] args) {
+	        // Criando uma instância de Pessoa para cada estuantes
+	        Pessoa pessoa = new Pessoa("Selline Julia Lima Soares", 18);
+
+	        // Exibindo as informações dos estudantes
+	        pessoa.exibirInformacoes();
+	    }
+
+}
